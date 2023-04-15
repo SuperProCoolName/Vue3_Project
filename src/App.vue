05f27845b19eb4b16c750dc5c14923324693f455
SuperProCoolName <minzatov.2004@mail.ru>
2023-04-15 12:43:42 +0300

    2.8

--- src/App.vue
+++ src/App.vue
@@ -30,7 +30,7 @@ export default {
       </v-list-item>
       <v-divider></v-divider>
       <v-list dense>
-        <v-list-item v-for="link in links" :key="link.title">
+        <v-list-item v-for="link in links" :key="link.title" :to="link.url">
           <template v-slot:prepend>
             <v-icon :icon="link.icon"></v-icon>
           </template>
@@ -42,7 +42,7 @@ export default {
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn text v-for="link in links" :key="link.title">
+        <v-btn v-for="link in links" :key="link.title" :to="link.url">
           <v-icon start :icon="link.icon"></v-icon>
           {{ link.title }}
         </v-btn>
