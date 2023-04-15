aef2990d963d569ce7f3cfe3dd7622705cff63c1
SuperProCoolName <minzatov.2004@mail.ru>
2023-04-15 22:33:58 +0300

    6.2

--- src/views/User/OrdersView.vue
+++ src/views/User/OrdersView.vue
@@ -1,7 +1,24 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      orders: [
+        {
+          id: "0",
+          name: "Kostya",
+          phone: "+7(978)000-00-05",
+          adId: "1",
+          done: true,
+        },
+        {
+          id: "1",
+          name: "Osman",
+          phone: "+7(978)000-00-06",
+          adId: "2",
+          done: false,
+        },
+      ],
+    };
   },
 };
 </script>
@@ -12,17 +29,20 @@ export default {
       <v-col cols="6" offset="3">
         <h1 class="text--secondary mb-3 mt-3">Orders</h1>
         <v-list subheader two-line flat>
-          <v-list-item>
+          <v-list-item v-for="order in orders" :key="order.id">
             <template v-slot:prepend>
               <v-list-item-action>
-                <v-checkbox color="primary"></v-checkbox>
+                <v-checkbox
+                  :input-value="order.done"
+                  color="primary"
+                ></v-checkbox>
               </v-list-item-action>
             </template>
-            <v-list-item-title>Notifications</v-list-item-title>
-            <v-list-item-subtitle>Allow notifications</v-list-item-subtitle>
+            <v-list-item-title>{{ order.name }}</v-list-item-title>
+            <v-list-item-subtitle>{{ order.phone }}</v-list-item-subtitle>
             <template v-slot:append>
               <v-list-item-action>
-                <v-btn class="primary">Open</v-btn>
+                <v-btn class="primary" :to="'/ad/' + order.adId">Open</v-btn>
               </v-list-item-action>
             </template>
           </v-list-item>
