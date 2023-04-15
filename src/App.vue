71b15d64e2bdbe5bafa9b0bc7eb56ce6c1feaa50
SuperProCoolName <minzatov.2004@mail.ru>
2023-04-15 12:33:00 +0300

    2.5

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,17 @@ export default {
   data() {
     return {
       drawer: false,
+      links: [
+        { title: "Login", icon: "mdi-lock", url: "/login" },
+        { title: "Registration", icon: "mdi-face", url: "/registration" },
+        {
+          title: "Orders",
+          icon: "mdi-bookmark-multiple-outline",
+          url: "/orders",
+        },
+        { title: "New ad", icon: "mdi-note-plus-outline", url: "/new" },
+        { title: "My ads", icon: "mdi-view-list-outline", url: "/list" },
+      ],
     };
   },
 };
@@ -17,28 +28,26 @@ export default {
           <v-list-item-subtitle> Учебный проект </v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
-
       <v-divider></v-divider>
       <v-list dense>
-        <v-list-item>
+        <v-list-item v-for="link in links" :key="link.title">
           <template v-slot:prepend>
-            <v-icon icon="mdi-train-variant"></v-icon>
+            <v-icon :icon="link.icon"></v-icon>
           </template>
-          <v-list-item-title>Link One</v-list-item-title>
+          <v-list-item-title>{{ link.title }}</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar app dark color="primary">
-      <v-app-bar-nav-icon @click="drawer = !drawer"> </v-app-bar-nav-icon>
-      <v-spacer> </v-spacer>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn>
-          <v-icon start icon="mdi-train-variant"></v-icon>
-          Link One
+        <v-btn text v-for="link in links" :key="link.title">
+          <v-icon start :icon="link.icon"></v-icon>
+          {{ link.title }}
         </v-btn>
-        <v-btn>Link Two</v-btn>
-        <v-btn>Link Three</v-btn>
       </v-toolbar-items>
     </v-app-bar>
+    <v-main> </v-main>
   </v-app>
 </template>
