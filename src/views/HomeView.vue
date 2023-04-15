36f6c8a35f813b8974d28a6beb0e1f49aff22d8e
SuperProCoolName <minzatov.2004@mail.ru>
2023-04-15 14:37:19 +0300

    5.4

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,17 +19,24 @@ export default {
         },
         {
           title: "Third",
-          desc: "Thitd Desc",
+          desc: "Third Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
           id: "3",
         },
         {
-          title: "Fouth",
-          desc: "Fouth Desc",
+          title: "Fourth",
+          desc: "Fourth Desc",
           promo: true,
           src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
           id: "4",
+        },
+        {
+          title: "Fifth",
+          desc: "Fifth Desc",
+          promo: false,
+          src: "https://sun2.43222.userapi.com/impg/XaHn2WDZpufmHyeWMZi5ilR-qqIfrdl6nS86Ag/6SaOx8FrokI.jpg?size=221x89&quality=96&sign=ac9074db5b3496b3f18d92c16e1901cd&type=album",
+          id: "5",
         },
       ],
     };
@@ -42,7 +49,11 @@ export default {
     <v-row justify="center">
       <v-col cols="12" xs="12">
         <v-carousel>
-          <v-carousel-item cover></v-carousel-item>
+          <v-carousel-item v-for="ad in ads" :key="ad.id" :src="ad.src" cover>
+            <div class="ad-link">
+              <v-btn class="error" :to="'/ad/' + ad.id">{{ ad.title }}</v-btn>
+            </div></v-carousel-item
+          >
         </v-carousel>
       </v-col>
     </v-row>
@@ -68,3 +79,16 @@ export default {
     </v-row>
   </v-container>
 </template>
+
+<style scoped>
+.ad-link {
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  background: rgb(0, 0, 0, 0.5);
+  transform: translate(-50%, 0);
+  padding: 5px 15px;
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
+}
+</style>
