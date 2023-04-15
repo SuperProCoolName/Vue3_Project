f3ca0c5914a5554fe5abea59e35a555c64f4a25e
SuperProCoolName <minzatov.2004@mail.ru>
2023-04-15 12:25:08 +0300

    2.4

--- src/App.vue
+++ src/App.vue
@@ -10,10 +10,27 @@ export default {
 
 <template>
   <v-app>
-    <v-navigation-drawer app v-model="drawer"></v-navigation-drawer>
+    <v-navigation-drawer app v-model="drawer">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title"> КИПУ </v-list-item-title>
+          <v-list-item-subtitle> Учебный проект </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+      <v-list dense>
+        <v-list-item>
+          <template v-slot:prepend>
+            <v-icon icon="mdi-train-variant"></v-icon>
+          </template>
+          <v-list-item-title>Link One</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     <v-app-bar app dark color="primary">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-spacer></v-spacer>
+      <v-app-bar-nav-icon @click="drawer = !drawer"> </v-app-bar-nav-icon>
+      <v-spacer> </v-spacer>
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn>
           <v-icon start icon="mdi-train-variant"></v-icon>
@@ -23,6 +40,5 @@ export default {
         <v-btn>Link Three</v-btn>
       </v-toolbar-items>
     </v-app-bar>
-    <v-main></v-main>
   </v-app>
 </template>
