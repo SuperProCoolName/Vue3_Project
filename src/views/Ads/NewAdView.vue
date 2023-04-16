93e485cc634aedc1ba3a425f8869532d7c998ac4
SuperProCoolName <minzatov.2004@mail.ru>
2023-04-16 13:19:55 +0300

    7.2

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -1,7 +1,25 @@
 <script>
 export default {
+  name: "NewAdView",
   data() {
-    return {};
+    return {
+      valid: false,
+      title: "",
+      description: "",
+      promo: false,
+    };
+  },
+  methods: {
+    createAd() {
+      if (this.$refs.form.validate()) {
+        const ad = {
+          title: this.title,
+          desc: this.description,
+          promo: this.promo,
+        };
+        console.log(ad);
+      }
+    },
   },
 };
 </script>
@@ -33,7 +51,7 @@ export default {
           <v-col cols="8">
             <v-btn class="mt-3" color="warning">
               Upload
-              <v-icon right dark>mdi-cloud-upload</v-icon>
+              <v-icon right dark>mdi-cloud- upload</v-icon>
             </v-btn>
           </v-col>
         </v-row>
