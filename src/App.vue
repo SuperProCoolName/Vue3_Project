5f7cab104749d245b7aaf11cef8031b33a52c9db
SuperProCoolName <minzatov.2004@mail.ru>
2023-05-07 13:25:29 +0300

    fix

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,15 @@ export default {
     return {
       drawer: false,
       links: [
-        { title: "Login", icon: "mdi-lock", url: "/login" },
-        { title: "Registration", icon: "mdi-face", url: "/registration" },
+        { title: "Войти", icon: "mdi-lock", url: "/login" },
+        { title: "Зарегистрироваться", icon: "mdi-face", url: "/registration" },
         {
-          title: "Orders",
+          title: "Пользователи",
           icon: "mdi-bookmark-multiple-outline",
           url: "/orders",
         },
-        { title: "New ad", icon: "mdi-note-plus-outline", url: "/new" },
-        { title: "My ads", icon: "mdi-view-list-outline", url: "/list" },
+        { title: "Новинки", icon: "mdi-note-plus-outline", url: "/new" },
+        { title: "Корзина", icon: "mdi-view-list-outline", url: "/list" },
       ],
     };
   },
@@ -62,12 +62,14 @@ export default {
       </v-toolbar-items>
     </v-app-bar>
     <v-main>
-      <v-snackbar v-model="error" multi-line :timeout="2000" color="primary">
-        {{ error }}
-        <template v-slot:actions>
-          <v-btn variant="text" @click="closeError"> Close </v-btn>
-        </template>
-      </v-snackbar>
+      <router-view></router-view>
     </v-main>
+    <v-snackbar v-model="error" multi-line :timeout="2000" color="primary">
+      {{ error }}
+      <template v-slot:actions>
+        <v-btn variant="text" @click="closeError"> Close </v-btn>
+      </template>
+    </v-snackbar>
   </v-app>
 </template>
+<style scoped></style>
