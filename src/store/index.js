bde068d033f50eefa99004fa7bfbe1a4cd0accd8
SuperProCoolName <minzatov.2004@mail.ru>
2023-06-17 12:11:58 +0300

    18.5

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,10 @@ import { createStore } from "vuex";
 import ads from "./ads";
 import user from "./user";
 import shared from "./shared";
+import orders from "./orders";
 
 const store = createStore({
-  modules: { ads, user, shared },
+  modules: { ads, user, shared, orders },
 });
 
 export default store;
